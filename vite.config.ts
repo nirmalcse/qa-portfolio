a878eec1400b9764c521223416ca93e5dd4078d2
nirmalkumar <nirmalspecial@gmail.com>
2026-05-07 18:50:36 +0530

    base url updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(async () => {
-  base: "/qa-portfolio/";
   const plugins = [react(), tailwindcss()];
+
   try {
     // @ts-ignore
-    
     const m = await import('./.vite-source-tags.js');
     plugins.push(m.sourceTags());
   } catch {}
-  return { plugins };
-})
+
+  return {
+    base: '/qa-portfolio/', // ✅ NOW it is applied
+    plugins
+  };
+});
